18ce987d18b3a50e048854afcd0e1ed7082d64cd
mario welch <mario.welch01@gmail.com>
2022-01-31 15:16:50 -0600

    joins exercise

---
+++ join_exercises.sql
@@ -0,0 +1,57 @@
+USE employees;
+
+#Shows each department along with the name of the current manager for that department
+SELECT departments.dept_name AS Department_Name, CONCAT(employees.first_name, ' ', employees.last_name) AS Department_Manager
+FROM employees
+        JOIN dept_manager
+            ON dept_manager.emp_no = employees.emp_no
+        JOIN departments
+            ON departments.dept_no = dept_manager.dept_no
+WHERE dept_manager.to_date LIKE '9999%';
+
+#Shows all departments currently managed by women
+SELECT departments.dept_name AS Department_Name, CONCAT(employees.first_name, ' ', employees.last_name) AS Department_Manager
+FROM employees
+        JOIN dept_manager
+            ON dept_manager.emp_no = employees.emp_no
+         JOIN departments
+            ON departments.dept_no = dept_manager.dept_no
+WHERE dept_manager.to_date LIKE '9999'
+AND employees.gender = 'F';
+
+#Shows current titles of employees currently working in the Customer Service department
+SELECT titles.title AS title, COUNT(titles.title) AS Total
+FROM titles
+         JOIN dept_emp
+              ON dept_emp.emp_no = titles.emp_no
+WHERE dept_emp.dept_no = 'd009'
+AND dept_emp.to_date LIKE '9999%'
+AND titles.to_date LIKE '9999%'
+GROUP BY titles.title;
+
+#Shows current salary of all current managers
+SELECT departments.dept_name AS Department_Name, CONCAT(employees.first_name, ' ', employees.last_name) AS Department_Manager, salaries.salary AS Salary
+FROM employees
+        JOIN salaries
+            ON salaries.emp_no = employees.emp_no
+        JOIN dept_manager
+            ON dept_manager.emp_no = salaries.emp_no
+        JOIN departments
+            ON departments.dept_no = dept_manager.dept_no
+WHERE dept_manager.to_date LIKE '9999%'
+AND salaries.to_date LIKE '9999%';
+
+#Bonus
+#Shows the names of all current employees, their department name, and their current managers name
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS Employee_Name, departments.dept_name AS Department_Name, CONCAT(mgmt.first_name, ' ', mgmt.last_name) AS Manager
+FROM employees
+    JOIN dept_emp
+        ON dept_emp.emp_no = employees.emp_no
+    JOIN departments
+        ON departments.dept_no = dept_emp.dept_no
+    JOIN dept_manager
+        ON dept_manager.dept_no = departments.dept_no
+    JOIN employees AS mgmt
+        ON mgmt.emp_no = dept_manager.emp_no
+WHERE dept_manager.to_date LIKE '9999%'
+AND dept_emp.to_date LIKE '9999%';
---
+++ join_test.sql
@@ -0,0 +1,51 @@
+USE join_test_db;
+
+CREATE TABLE roles (
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       name VARCHAR(100) NOT NULL,
+                       PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       name VARCHAR(100) NOT NULL,
+                       email VARCHAR(100) NOT NULL,
+                       role_id INT UNSIGNED DEFAULT NULL,
+                       PRIMARY KEY (id),
+                       FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('author');
+INSERT INTO roles (name) VALUES ('reviewer');
+INSERT INTO roles (name) VALUES ('commenter');
+
+INSERT INTO users (name, email, role_id)
+VALUES ('bob', 'bob@example.com', 1),
+        ('joe', 'joe@example.com', 2),
+        ('sally', 'sally@example.com', 3),
+        ('adam', 'adam@example.com', 3),
+        ('jane', 'jane@example.com', null),
+        ('mike', 'mike@example.com', null);
+
+INSERT INTO users (name, email, role_id)
+VALUES ('mario', 'mario@example.com', 2),
+       ('maverick', 'maverick@example.com', 2),
+       ('alexis', 'alexis@example.com', 2),
+       ('keith', 'keith@example.com', null);
+
+
+#JOIN
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+JOIN roles on users.role_id = roles.id;
+
+#LEFT JOIN
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+LEFT JOIN roles on users.role_id = roles.id;
+
+#RIGHT JOIN
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+RIGHT JOIN roles on users.role_id = roles.id;
